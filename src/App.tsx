ea7917f49b07299a7f1ebb7f0a684bdbb42b4641
Enif Lee <enif.lee@gmail.com>
2021-04-09 01:54:10 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,89 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.css'
+import 'rsuite/lib/styles/index.less';
+import {
+    Col,
+    Content,
+    Divider,
+    Dropdown,
+    FlexboxGrid,
+    Footer,
+    Grid,
+    Header, Icon,
+    Input,
+    InputGroup,
+    Nav,
+    Navbar,
+    Row
+} from "rsuite";
+import styled from "@emotion/styled";
+import {css} from "@emotion/css";
+import InputGroupAddon from "rsuite/es/InputGroup/InputGroupAddon";
+
+
+const MainNav = styled(Nav)`
+  min-width: 50%;
+  margin: 0 auto;
+  float: none;
+  text-align: center;
+`
+
+const MainTitle = styled.h1`
+  font-size: 3rem;
+`
+
+const MainSubTitle = styled.h2`
+  font-size: 1rem;
+  margin-bottom: 0.5rem;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <Header>
+                <Navbar>
+                    <Navbar.Body>
+                        <MainNav>
+                            <Dropdown title="문서"><Dropdown.Item>Contact</Dropdown.Item></Dropdown>
+                            <Dropdown title="개발툴 및 협업툴"><Dropdown.Item>Contact</Dropdown.Item></Dropdown>
+                            <Dropdown title="UI 및 UX"><Dropdown.Item>Contact</Dropdown.Item></Dropdown>
+                            <Dropdown title="그래픽디자인"><Dropdown.Item>Contact</Dropdown.Item></Dropdown>
+                            <Dropdown title="3D/AR"><Dropdown.Item>Contact</Dropdown.Item></Dropdown>
+                            <Dropdown title="사진"><Dropdown.Item>Contact</Dropdown.Item></Dropdown>
+                            <Dropdown title="비디오"><Dropdown.Item>Contact</Dropdown.Item></Dropdown>
+                            <Divider vertical={true} className={css`
+                              height: 2rem;
+                              margin: .75rem 0.25rem;
+                            `}/>
+                            <Nav.Item>관련 단축키 팁</Nav.Item>
+                            <Nav.Item>설정</Nav.Item>
+                        </MainNav>
+                    </Navbar.Body>
+                </Navbar>
+            </Header>
+            <Content>
+                <FlexboxGrid justify={"center"} align={"middle"} className={css`height: 600px; text-align: center;`}>
+                    <FlexboxGrid.Item colspan={16}>
+                        <img src={"https://via.placeholder.com/240"} alt={"logo"} className={css`padding: 10px;`}/>
+                        <MainTitle>We make you find a Hotkey.</MainTitle>
+                        <MainSubTitle>단축키를 쓰면 업무 효율이 팡팡올라욧.</MainSubTitle>
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+                        <InputGroup className={css`max-width: 480px; margin: 0 auto;border-radius: 2rem; overflow: hidden`}>
+                            <InputGroupAddon className={css`background: none`}>
+                                <Icon icon={"search"}/>
+                            </InputGroupAddon>
+                            <Input className={css`border-radius: 2rem;`}/>
+                        </InputGroup>
+                    </FlexboxGrid.Item>
+                </FlexboxGrid>
+
+            </Content>
+            <Footer>
+                푸터야
+            </Footer>
+        </>
+
+    )
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  },
+  define: {
+    'process.env': {}
+  }
 })
